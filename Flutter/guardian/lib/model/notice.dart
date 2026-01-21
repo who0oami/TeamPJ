113f795ddc8b9d713018e997c5a4c102f9c12c8c
hmw0320 <hmw010320@gmail.com>
2026-01-21 17:06:11 +0900

    Hmw (#67)
    * homework, notice, timetable model
    
    * homework, notice, timetable model 2
    
    * 1/19 hmw
    
    * 1/20
    
    * 1/20 2
    
    * 1/21 hmw

--- Flutter/guardian/lib/model/notice.dart
+++ Flutter/guardian/lib/model/notice.dart
@@ -7,20 +7,20 @@ Author : 정시온
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Notice {
-  final String notice_id;      // Firestore doc.id
-  final String teacher_id;    // FK
+  final String? notice_id;       // Firestore doc.id
+  final int teacher_id;    // FK
   final String notice_title; 
   final String notice_content;   
-  final DateTime notice_updatedate;
+  final DateTime? notice_updatedate;
   final DateTime notice_insertdate;
-  final String notice_images;   
+  final List<String> notice_images;   
 
   Notice({
-    required this.notice_id,
+    this.notice_id,
     required this.teacher_id,
     required this.notice_title,
     required this.notice_content,
-    required this.notice_updatedate,
+    this.notice_updatedate,
     required this.notice_insertdate,
     required this.notice_images,
   });
@@ -28,12 +28,23 @@ class Notice {
   factory Notice.fromMap(Map<String, dynamic> map, String id){
     return Notice(
       notice_id: id,
-      teacher_id: map['teacher_id'] ?? "",
+      teacher_id: map['teacher_id'] ?? 0,
       notice_title: map['notice_title'] ?? "",
       notice_content: map['notice_content'] ?? "",
-      notice_updatedate: (map['notice_updatedate'] as Timestamp).toDate(),
+      notice_updatedate: map['notice_updatedate'] != null ? (map['notice_updatedate'] as Timestamp).toDate() : null,
       notice_insertdate: (map['notice_insertdate'] as Timestamp).toDate(),
-      notice_images: map['notice_images'] ?? "",
+      notice_images: List<String>.from(map['notice_images'] ?? []),
     );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'teacher_id': teacher_id,
+      'notice_title': notice_title,
+      'notice_content': notice_content,
+      'notice_insertdate': Timestamp.fromDate(notice_insertdate),
+      'notice_updatedate': notice_updatedate != null ? Timestamp.fromDate(notice_updatedate!) : null,
+      'notice_images': notice_images,
+    };
   }
 }

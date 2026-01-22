ee3ac6392c420cd6a801a2c4b2c6cdaa37061cb0
hmw0320 <hmw010320@gmail.com>
2026-01-22 18:02:32 +0900

    Hmw (#80)
    * homework, notice, timetable model
    
    * homework, notice, timetable model 2
    
    * 1/19 hmw
    
    * 1/20
    
    * 1/20 2
    
    * 1/21 hmw
    
    * 1/22 hmw

--- Flutter/guardian/lib/model/lunch.dart
+++ Flutter/guardian/lib/model/lunch.dart
@@ -1,33 +1,35 @@
 /* 
-Description : Lunch 테이블 구성
-Date : 2026-1-16
-Author : 시온
+Description : Lunch Model for Firebase
+Date : 2026-1-22
+Author : 황민욱
 */
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Lunch{
-  final String lunch_id;
-  final String lunch_category_id;
-  final DateTime lunch_date;
-  final String lunch_menu_id;
+class Lunch {
+  final String lunch_id;                           // Firestore doc.id
+  final DateTime lunch_date;                       // 급식 날짜
+  final Map<String, List<String>> lunch_contents;  // 카테고리별 menuId 리스트
 
-  Lunch(
-    {
-      required this.lunch_id,
-      required this.lunch_category_id,
-      required this.lunch_date,
-      required this.lunch_menu_id,
-    }
+  Lunch({
+    required this.lunch_id,
+    required this.lunch_date,
+    required this.lunch_contents,
+  });
 
-  );
-  factory Lunch.fromMap(Map<String, dynamic> map, String id){
+  factory Lunch.fromMap(Map<String, dynamic> map, String id) {
     return Lunch(
       lunch_id: id,
-      lunch_category_id: map['lunch_category_id'] ?? "",
-      lunch_date: (map['lunch_date']as Timestamp).toDate(),
-      lunch_menu_id: map['lunch_menu_id'] ?? ""
+      lunch_date: (map['lunch_date'] as Timestamp).toDate(),
+      lunch_contents: (map['lunch_contents'] as Map<String, dynamic>)
+          .map((key, value) => MapEntry(key, List<String>.from(value as List))),
     );
   }
 
+  Map<String, dynamic> toMap() {
+    return {
+      'lunch_date': Timestamp.fromDate(lunch_date),
+      'lunch_contents': lunch_contents,
+    };
+  }
 }
